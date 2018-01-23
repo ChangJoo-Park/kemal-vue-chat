ac20b56b523675f59b34657918cb370251b7b0fc
ChangJoo Park <changjoo.park@partner.so>
2018-01-23 11:45:57 +0900

    first version of chat implementations

---
+++ src/assets/javascripts/main.js
@@ -0,0 +1,97 @@
+new Vue({
+  el: '#app',
+  template: `
+    <div v-if="connected">
+      <h1 class="title">Kemal, Vue.js chat <small v-if="username"> - {{ username }}</small></h1>
+      <div class="message-wrapper">
+        <div class="message-box">
+          <div class="message" v-for="message in messages">
+            <strong class="message-username">{{ message.username }}</strong> : <span class="message-body">{{ message.body }}</span>
+          </div>
+        </div>
+      </div>
+      <div class="input-wrapper">
+        <input class="message-input" type="text" @keyup.enter="onInputEnter" v-model="message" autofocus>
+      </div>
+    </div>
+    <div v-else>
+      <h1>Please refresh browser for join a room.</h1>
+    </div>
+  `,
+  data: function () {
+    return {
+      connected: false,
+      username: '',
+      messages: [],
+      message: '',
+      server: null,
+      sendable: true
+    }
+  },
+  // Life Cycles
+  created: function () {
+  },
+  mounted: function () {
+    console.log('Hello Vue.js - Kemal Chat')
+    this.username = window.prompt('What is your name ?')
+    if (this.username.trim() === '') {
+      window.alert('Please refresh tab for username')
+      return
+    }
+    this.openWebSocket()
+  },
+  beforeDestroy: function () {
+    this.closeWebSocket()
+  },
+  // Methods
+  methods: {
+    onInputEnter: function () {
+      if (this.message.trim() === '') {
+        return 
+      }
+
+      setTimeout(function () {
+        this.sendable = true
+      }.bind(this), 100);
+
+      // Send Message
+      if (this.sendable) {
+        this.sendable = false
+        this.sendMesssage(this.message)
+        this.message = ''
+      }
+    },
+    sendMesssage: function (message) {
+      this.server.send(`${this.username}:${message}`)
+    },
+    openWebSocket: function () {
+      const server = new WebSocket(`ws://${location.hostname}:${location.port}`)
+      server.onmessage = (event) => {
+        const serverSentMessages = JSON.parse(event.data)
+        this.messages = serverSentMessages.map(m => {
+          const splitted = m.split('\:')
+          if (splitted.length < 2) {
+            return { username: '', message: '' } // invalid message arrived
+          }
+          const username = splitted[0]
+          return {
+            username: username,
+            body: m.replace(`${username}:`, '')
+          }
+        })
+
+        this.$nextTick(this.scrollToBottom)
+      }
+
+      server.onopen = () => {
+        this.connected = true
+        this.sendMesssage(`has join this room ${new Date()} `)
+      }
+
+      this.server = server
+    },
+    scrollToBottom: function () {
+      document.querySelector('.message-wrapper').scrollTo(0, document.querySelector(".message-box").scrollHeight)
+    }
+  }
+})
--- src/kemal-vue-chat.cr
+++ src/kemal-vue-chat.cr
@@ -1,7 +1,29 @@
 require "kemal"
 
+messages = [] of String
+sockets = [] of HTTP::WebSocket
+
+public_folder "src/assets"
+
 get "/" do
-  "Hello World!"
+  render "src/views/index.ecr"
+end
+
+ws "/" do |socket|
+  sockets.push socket
+
+  # Handle incoming message and dispatch it to all connected clients
+  socket.on_message do |message|
+    messages.push message
+    sockets.each do |a_socket|
+      a_socket.send messages.to_json
+    end
+  end
+
+  socket.on_close do |_|
+    sockets.delete socket
+    puts "Closing Socket: #{socket}"
+  end
 end
 
 Kemal.run
